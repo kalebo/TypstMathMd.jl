e7acf2240849a8a77fa55bd08940f30cadb385f2
kalebo <kaleb@coreform.com>
2024-11-09 21:46:28 -0700

    Support julia string interpolation

--- src/TypstMathMd.jl
+++ src/TypstMathMd.jl
@@ -6,7 +6,7 @@ module TypstMathMd
   export @typstmd_str
 
   function convert_typst_multi(content::AbstractString)
-    trimmed_content = String(strip(content, ['%', ' ']))
+    trimmed_content = String(strip(content, ['`', ' ']))
     is_inline = trimmed_content[1] != '\n'
 
     processed = run(Pandoc.Converter(; input = string('$', trimmed_content, '$'), from="typst", to="markdown"))
@@ -20,9 +20,14 @@ module TypstMathMd
   end
 
   macro typstmd_str(str)
-    pattern = r"%%(.+?)%%"s  # 's' flag for dotall mode (. matches newlines)
-    processed = replace(str, pattern => s -> convert_typst_multi(s))
-    return :(Markdown.parse($processed))
+	  str_expr = Meta.parse("\"\"\"$str\"\"\"")
+	  quote
+	  	let
+        interpolated = $(str_expr)
+  		  pattern = r"``(.+?)``"s  # 's' flag for dotall mode (. matches newlines)
+    		processed = replace(interpolated, pattern => s -> convert_typst_multi(s))
+    		Markdown.parse(processed)
+		  end
+	  end
   end
-
 end
